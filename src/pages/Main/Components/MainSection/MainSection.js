95fb310c59c7015b2bf8d065a1844cd0febdee20
Daseul <dls2tmfs2@gmail.com>
2021-05-12 15:12:21 +0900

    ADD : fetch

--- src/pages/Main/Components/MainSection/MainSection.js
+++ src/pages/Main/Components/MainSection/MainSection.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import MainSectionMovieList from '../MainSectionMovieList/MainSectionMovieList';
 import './MainSection.scss';
-
 export default class MainSection extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movieInfomationList: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('받을 API 주소', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(movieInfomation => {
+        this.setState({
+          movieInfomationList: movieInfomation,
+        });
+      });
+  }
+
   render() {
     return (
       <section className="mainSection">
@@ -10,10 +28,21 @@ export default class MainSection extends React.Component {
           <p>박스오피스 순위</p>
         </div>
         <section className="mainMovie">
-          <MainSectionMovieList />
-          <MainSectionMovieList />
-          <MainSectionMovieList />
-          <MainSectionMovieList />
+          <button>이전</button>
+          <ul>
+            {movieInfomationList.map(movie => {
+              return (
+                <MainSectionMovieList
+                  id={movie.id}
+                  title={movie.korean_title}
+                  country={movie.country}
+                  releaseDate={movie.release_date}
+                  thumbnailImg={movie.thumbnail_img}
+                />
+              );
+            })}
+          </ul>
+          <button>다음</button>
         </section>
       </section>
     );
--- src/pages/Main/Components/MainSectionMovieList/MainSectionMovieList.js
+++ src/pages/Main/Components/MainSectionMovieList/MainSectionMovieList.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './MainSectionMovieList.scss';
-
 export default class MainSectionMovieList extends React.Component {
   render() {
+    const { id, korean_title, country, release_date } = this.props;
     return (
-      <section className="mainSectionMovieList">
+      <li className="mainSectionMovieList">
         <div className="listRanking">1</div>
         <img
           alt="test"
           src="https://movie-phinf.pstatic.net/20210421_37/1618971733493B4ykS_JPEG/movie_image.jpg"
         />
         <div className="listDes">
-          <p className="listDesTitle">분노의 질주: 더 얼티메이트</p>
-          <p className="listDesYear">2020</p>
+          <p className="listDesTitle">{korean_title}</p>
+          <p className="listDesYear">{country}</p>
           <p className="listDesGrade">평균 3.8</p>
         </div>
-      </section>
+      </li>
     );
   }
 }
