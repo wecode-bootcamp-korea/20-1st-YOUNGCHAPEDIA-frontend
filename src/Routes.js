157b58464d6702e86205804a4d9e51641f57534e
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-13 00:42:21 +0900

    Add : 리뷰페이지(평가페이지) 레이아웃 구현, 무한스크롤 구현 (Mock Data)

--- src/Routes.js
+++ src/Routes.js
@@ -6,7 +6,7 @@ import Footer from './pages/CommonComponents/Footer';
 // import login
 // import mypage
 import Navbar from './pages/CommonComponents/Navbar';
-import EvaluationPage from './pages/EvaluationPage/EvaluationPage';
+import ReviewPage from './pages/ReviewPage/ReviewPage';
 
 class Routes extends React.Component {
   render() {
@@ -15,12 +15,11 @@ class Routes extends React.Component {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Main}></Route>
-          <Route exact path="/review" component={EvaluationPage}></Route>
-
+          <Route exact path="/review" component={ReviewPage}></Route>
           {/* <Route exact path="/contents" component={}></Route> */}
           {/* <Route exact path="/myPage" component={}></Route> */}
         </Switch>
-        <Footer />
+        {/* <Footer /> */}
       </Router>
     );
   }
