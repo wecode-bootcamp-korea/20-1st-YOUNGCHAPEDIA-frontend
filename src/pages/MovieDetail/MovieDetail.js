89b8450443d3bac75b229053c95afb2536d6a3df
Daseul <dls2tmfs2@gmail.com>
2021-05-19 10:09:31 +0900

    동적라우팅 백업

--- src/pages/MovieDetail/MovieDetail.js
+++ src/pages/MovieDetail/MovieDetail.js
@@ -6,11 +6,26 @@ import MovieDetailContentsSection from './Components/MovieDetailContentsSection/
 import './MovieDetail.scss';
 
 export default class MovieDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { movieInformation: {} };
+  }
+
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    fetch(`http://10.58.5.25:8000/movies/${id}`)
+      .then(res => res.json())
+      .then(res => this.setState({ movieInformation: res }));
+  }
+
   render() {
-    console.log(this.props.location);
+    console.log(this.props.match.params.id);
+    const { movieInformation } = this.state;
+    console.log(movieInformation);
+
     return (
       <main className="MovieDetail">
-        <MovieBannerSection />
+        <MovieBannerSection movieInformation={movieInformation} />
         <LeaveCommentSection />
         <MovieDetailContentsSection />
       </main>
